d1bfc744893f577cc47591547925edd743b5fe30
Nobert Jakpor <jakpornobert@gmail.com>
2021-08-16 16:24:59 +0100

    Initial Commit

---
+++ database.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from index import app
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Users.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    email = db.Column("email", db.String(100))
+    password = db.Column("password")
