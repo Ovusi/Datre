9d5a1c16b99e452a2b03611f6f059b0e2a928a02
Nobert Jakpor <jakpornobert@gmail.com>
2021-08-17 13:10:29 +0100

    Initial Commit

--- index.py
+++ index.py
@@ -2,37 +2,31 @@ from flask import Flask, render_template, url_for
 import database
 
 app = Flask(__name__)
-db = database.db
 
 
 @app.route("/")
 def index():
-    # return render_template("")
-    pass
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    # return render_template("")
-    pass
+    return render_template("index.html")
 
 
 @app.route("/login")
 def login():
-    # return render_template("")
-    pass
+    return render_template("index.html")
 
 
 @app.route("/signup")
 def signup():
-    # return render_template("")
-    pass
+    return render_template("index.html")
 
 
 @app.route("/<user>")
 def user():
-    # return render_template("")
-    pass
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
