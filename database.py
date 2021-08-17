cbb1269c0325bf7cd509ec4549b0ea0738534cde
Nobert Jakpor <jakpornobert@gmail.com>
2021-08-17 17:03:14 +0100

    Initial Commit

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from server import db
 
 class Users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column("name", db.String(100))
+    name = db.Column("name", db.String(32))
     email = db.Column("email", db.String(100))
     password = db.Column("password", db.String(12))
     phone = db.Column("phone", db.Integer)
+    data_file = db.Column("data_file", db.LargeBinary)
