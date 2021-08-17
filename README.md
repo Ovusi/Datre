4b701ab8b9dcc51a246bf81fef831b6a5097126d
Nobert Jakpor <jakpornobert@gmail.com>
2021-08-17 10:21:45 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### **DATRE**
+
+Datre is a simple data analysis web application which ingests data files (xlsx, csv), processes it and displays the data
+graphically in charts and tables.
--- index.py
+++ index.py
@@ -1,4 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    # return render_template("")
+    pass
+
+
+@app.route("/about")
+def about():
+    # return render_template("")
+    pass
+
+
+@app.route("/login")
+def login():
+    # return render_template("")
+    pass
+
+
+@app.route("/signup")
+def signup():
+    # return render_template("")
+    pass
+
+
+@app.route("/<user>")
+def user():
+    # return render_template("")
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
