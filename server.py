bb8104816691fd2eef8f2e7fe2dbf1ccdd2cb303
Nobert Jakpor <jakpornobert@gmail.com>
2021-08-18 10:27:48 +0100

    Initial Commit

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.py"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Users.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
