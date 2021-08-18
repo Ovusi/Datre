e6570fe7658920fad75027e559e5bcf2bb9adff2
Nobert Jakpor <jakpornobert@gmail.com>
2021-08-18 09:17:16 +0100

    Initial Commit

--- logic.py
+++ logic.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from flask_admin import Admin
+from flask_login import LoginManager, UserMixin
+from flask_security import Security
 from openpyxl import load_workbook
 
 
@@ -11,3 +14,19 @@ def xlsx(file):
         pass
     for col in sheet.iter_cols(min_row=1, max_row=2, min_col=1, max_col=3, values_only=True):
         pass
+
+
+def login():
+    pass
+
+
+def logout():
+    pass
+
+
+def admin():
+    pass
+
+
+def download():
+    pass
--- server.py
+++ server.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, url_for
 from database import *
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Users.sqlite3"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.py"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
