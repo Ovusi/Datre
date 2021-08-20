ef73df80b09ab0db73acee3559c2baa060cae7a9
Nobert Jakpor <jakpornobert@gmail.com>
2021-08-20 16:59:27 +0100

    Initial Commit

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ import os
 
 class Users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column("name", db.String(32))
-    email = db.Column("email", db.String(100))
+    name = db.Column("name", db.String(32), unique=True)
+    email = db.Column("email", db.String(100), unique=True)
     password = db.Column("password", db.String(12))
     phone = db.Column("phone", db.Integer)
 
