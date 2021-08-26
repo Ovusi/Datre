3f9e28b419abce8dc6f3c6e9fa5c2df4e9c32a48
Nobert Jakpor <jakpornobert@gmail.com>
2021-08-26 11:40:59 +0100

    Initial Commit

--- app.py
+++ app.py
@@ -18,11 +18,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("")
-
-
 @app.route("/login")
 def login():
     user_name = request.form[""]
